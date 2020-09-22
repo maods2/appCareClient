fea998429ce46bdfa92864399782f4cb22358be0
Matheus Augusto <matheusalgusto_@hotmail.com>
2020-09-21 22:40:29 -0300

    perfil adicionado

--- src/pages/Patient/Perfil/index.js
+++ src/pages/Patient/Perfil/index.js
@@ -1,10 +1,32 @@
 import React, { useState, useEffect, useContext } from 'react';
 import PageHeader from '../../../components/PageHeader';
 
-// import './styles.css';
+
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography'
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import Container from '@material-ui/core/Container';
 import AuthService from '../../../services/auth.service'
 
-
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 752,
+  },
+  demo: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  title: {
+    margin: theme.spacing(4, 0, 2),
+  },
+}));
 
 
 export default function Perfil() {
@@ -18,7 +40,9 @@ export default function Perfil() {
     setUserData(firstName);
   }, []);
 
-
+  const classes = useStyles();
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
 
 
   return (
@@ -26,12 +50,66 @@ export default function Perfil() {
 
     <div align="center">
       { userData && <div id="page-cuidadores" className="container">
-      <PageHeader name={userData} />
+        <PageHeader name={userData} />
+
+
+        <React.Fragment>
+          <CssBaseline />
+          <Container maxWidth="sm">
+          <List className={classes.root}>
+
+          <ListItem alignItems="flex-start">
+            <ListItemText
+              primary="Primeiro Nome"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    component="span"
+                    variant="body2"
+                    className={classes.inline}
+                    color="textPrimary"
+                  >
+        
+              </Typography>
+                  {" Matheus Augusto"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+
+
+          <ListItem alignItems="flex-start">
+            <ListItemText
+              primary="Primeiro Nome"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    component="span"
+                    variant="body2"
+                    className={classes.inline}
+                    color="textPrimary"
+                  >
+        
+              </Typography>
+                  {" Matheus Augusto"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+
+
+        </List>
+          </Container>
+        </React.Fragment>
+
+
+
+
+
 
       </div>}
-
-
-
     </div>
 
   );
